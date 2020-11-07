847305a4d3c318ec7a5cba0ea6b0828260a18d03
pricee <emmalinprice@gmail.com>
2020-11-06 22:03:38 -0500

    reposition stores based on screen width, clear up dev console

--- src/App.js
+++ src/App.js
@@ -4,35 +4,61 @@ import Grid from '@material-ui/core/Grid';
 const { ratedStores, unratedStores } = require('./stores1');
 
 function App() {
+  const screenWidth = useWindowSize();
+
   return (
     <div className="App">
       <h1 className="site-title">I Want Fries</h1>
       <div className="site-subtitle">Because, let's be honest, all we really want are fries</div>
       <Grid container className="stores" direction="column" spacing={0}>
-          {renderStores(ratedStores)}
-          {renderStores(unratedStores)}
+          {renderStores(ratedStores, screenWidth)}
+          {renderStores(unratedStores, screenWidth)}
       </Grid>
-      <div className="bottom-text">By <a href="https://github.com/pricee/i-want-fries" target="_blank">Emma Price</a></div>
+      <div className="bottom-text">By <a href="https://github.com/pricee/i-want-fries" target="_blank" rel="noreferrer">Emma Price</a></div>
+      <div>Think I'm wrong? Have a recommendation? Email emmalinprice@gmail.com</div>
     </div>
   );
 }
 
+function useWindowSize() {
+    const [width, setWidth] = React.useState(0);
+    React.useLayoutEffect(() => {
+        function updateSize() {
+            setWidth(window.innerWidth);
+        }
+        window.addEventListener('resize', updateSize);
+        updateSize();
+        return () => window.removeEventListener('resize', updateSize);
+    }, []);
+    return width;
+}
 
-
-function renderStores(stores) {
+function renderStores(stores, screenWidth) {
     const storesGrid = [];
-    for (let i = 0; i < stores.length; i +=3) {
-        if (i + 3 > stores.length) {
+    let rowSize = 3;
+    if (screenWidth < 800) {
+        rowSize = 1;
+    } else if (screenWidth < 1200) {
+        rowSize = 2;
+    }
+    for (let i = 0; i < stores.length; i += rowSize) {
+        if (i + rowSize > stores.length) {
             storesGrid.push(stores.slice(i, stores.length));
         } else {
-            storesGrid.push(stores.slice(i, i + 3));
+            storesGrid.push(stores.slice(i, i + rowSize));
         }
     }
-    return storesGrid.map(row => {
+    return storesGrid.map((row, index) => {
         return (
-            <Grid className="row" container item justify="center" spacing={3}>
-                {row.map(store => {
-                    return <Grid className="single-store" key={store.Location} item xs={4}>
+            <Grid className="row" key={`row-${index}`} container item justify="center" spacing={3}>
+                {row.map((store, rowIndex) => {
+                    let spacing = 4;
+                    if (rowSize === 2) {
+                        spacing = 6;
+                    } else if (rowSize === 1) {
+                        spacing = 12;
+                    }
+                    return <Grid className="single-store" key={store.Location} item xs={spacing}>
                         <Store
                             storeName={store.Location}
                             address={store.Address}
@@ -58,9 +84,9 @@ function getPrice(price) {
     }
 }
 
-function showSizes(sizes) {
+function showSizes(sizes, location) {
     return sizes.map(sizeInfo =>
-            <div style={{fontSize:18}}><b>{sizeInfo.size + ": " + getPrice(sizeInfo.price)}</b></div>
+            <div style={{fontSize:18}} key={`${location}-${sizeInfo.size}`}><b>{sizeInfo.size + ": " + getPrice(sizeInfo.price)}</b></div>
     )
 }
 
@@ -71,7 +97,7 @@ function Store(props) {
               <Grid container item xs style={{ padding:15 }} justify="center">
                       <div style={{fontSize:30 }}><b>{props.storeName}</b></div>
               </Grid>
-              <Grid container item xs borderRadius={20} justify="center">
+              <Grid container item xs justify="center">
                   <div style={{ fontSize:20, paddingBottom:20 }}><b>Coming Soon...</b></div>
               </Grid>
           </Grid>);
@@ -87,8 +113,8 @@ function Store(props) {
                   {showSizes(props.sizes)}
               </Grid>
           </Grid>
-          <Grid container item xs borderRadius={20}>
-              <Grid className="store-section left" item xs={4} justify="center">
+          <Grid container item xs>
+              <Grid className="store-section left" container item xs={4} justify="center">
                   <div className="all-stats">
                       <div className="stats"><b><i>Thickness: </i></b>{props.thickness}</div>
                       <div className="stats"><b><i>Saltiness: </i></b>{props.saltiness}</div>
@@ -103,7 +129,7 @@ function Store(props) {
                   <div className="comments">{props.comments}</div>
               </Grid>
           </Grid>
-          <Grid className="address" item xs justify="center">
+          <Grid className="address" container item xs justify="center">
               <div><i>{props.address}</i></div>
           </Grid>
       </Grid>
